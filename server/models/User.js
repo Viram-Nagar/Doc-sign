947cb5ce2c49ce1dcfd34c38d0a749d77a8ffb99
Viram Nagar <viramnagar1019@gmail.com>
2026-06-15 11:41:12 +0530

    Initial commit — DocSign full-stack app

--- server/models/User.js
+++ server/models/User.js
@@ -29,10 +29,16 @@ const userSchema = new mongoose.Schema(
 );
 
 // Hash password before saving
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
+// userSchema.pre("save", async function (next) {
+//   if (!this.isModified("password")) return next();
+//   this.password = await bcrypt.hash(this.password, 12);
+//   next();
+// });
+
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+
   this.password = await bcrypt.hash(this.password, 12);
-  next();
 });
 
 // Compare password method
